f68c074453e4d783dbf8f180b190f9eefd5a5949
ricardo.ruiz <ricrui3@gmail.com>
2018-09-04 16:00:22 -0500

    Initial commit

---
+++ src/test/java/com/ricrui3/richpetclinic/RichPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ricrui3.richpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RichPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
