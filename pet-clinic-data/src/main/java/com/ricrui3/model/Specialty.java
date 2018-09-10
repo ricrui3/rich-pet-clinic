9aa63e2ee814b22427523c2a903a54b7be6bea49
ricardo.ruiz <ricrui3@gmail.com>
2018-09-10 15:09:38 -0500

    Created Vet speciality entity associate to vet. Closes #23

---
+++ pet-clinic-data/src/main/java/com/ricrui3/model/Specialty.java
@@ -0,0 +1,17 @@
+package com.ricrui3.model;
+
+/**
+ * Created By ricardo.ruiz
+ */
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
