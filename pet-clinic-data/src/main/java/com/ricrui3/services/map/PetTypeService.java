60af6ee39ba09c57875b051cae8540e839578870
ricardo.ruiz <ricrui3@gmail.com>
2018-09-10 15:29:24 -0500

    Created pet type map service impl. Closes #25

---
+++ pet-clinic-data/src/main/java/com/ricrui3/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.ricrui3.services.map;
+
+import com.ricrui3.model.PetType;
+import com.ricrui3.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
