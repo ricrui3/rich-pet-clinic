94e0fbe938abbae23f284bf16536422b8020b50c
ricardo.ruiz <ricrui3@gmail.com>
2018-09-10 15:44:57 -0500

    Added PetTypes in with Bootstrap (startup). Closes #28

--- pet-clinic-web/src/main/java/com/ricrui3/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ricrui3/bootstrap/DataLoader.java
@@ -1,9 +1,11 @@
 package com.ricrui3.bootstrap;
 
 import com.ricrui3.model.Owner;
+import com.ricrui3.model.PetType;
 import com.ricrui3.model.Vet;
 import com.ricrui3.services.OwnerService;
 import com.ricrui3.services.VetService;
+import com.ricrui3.services.map.PetTypeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +17,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args){
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Richard");
         owner1.setLastName("Ruiz");
