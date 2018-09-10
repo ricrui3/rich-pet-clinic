848166be350247900c88de6692c77927313b4d33
ricardo.ruiz <ricrui3@gmail.com>
2018-09-10 16:52:23 -0500

    Created specialty map service impl. Closes #27

---
+++ pet-clinic-data/src/main/java/com/ricrui3/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package com.ricrui3.services;
+
+import com.ricrui3.model.Specialty;
+
+/**
+ * Created By ricardo.ruiz
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
