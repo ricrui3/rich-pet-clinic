c63c59a649154ac002fd855fd19f05c6a32def36
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-07 16:47:57 -0600

    Updates base enttity with JPA annotations. Closes #31

--- pet-clinic-data/src/main/java/com/ricrui3/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ricrui3/model/BaseEntity.java
@@ -1,9 +1,19 @@
 package com.ricrui3.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+/**
+ * Created By Ricardo Ruiz on 07/01/18
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
