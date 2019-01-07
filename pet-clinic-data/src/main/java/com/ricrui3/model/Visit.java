63ac5c7e137a930969d553d70f68a50a339b54f7
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-07 17:26:42 -0600

    Added Visit Entity. closes #34

--- pet-clinic-data/src/main/java/com/ricrui3/model/Visit.java
+++ pet-clinic-data/src/main/java/com/ricrui3/model/Visit.java
@@ -1,16 +1,23 @@
 package com.ricrui3.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created By ricardo.ruiz
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
 
+    @Column(name = "description")
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
