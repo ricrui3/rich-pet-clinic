34c8afce926535d93b1ec96ebadcf1dd9c5a6e12
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-07 17:59:06 -0600

    added spring data jpa service for owners. closes #36

--- pet-clinic-data/src/main/java/com/ricrui3/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/ricrui3/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.ricrui3.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
