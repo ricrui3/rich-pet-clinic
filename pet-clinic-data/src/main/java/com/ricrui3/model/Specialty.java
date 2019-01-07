813e4b22fc2387b2f417cbf75274d4f989d3be02
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-07 17:17:37 -0600

    Updated Vet JPA. Closes #33

--- pet-clinic-data/src/main/java/com/ricrui3/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/ricrui3/model/Specialty.java
@@ -1,10 +1,17 @@
 package com.ricrui3.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created By ricardo.ruiz
  */
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
